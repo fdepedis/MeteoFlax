6cf7fc4d834908ba1b87848ee36fac92786ec422
fdepedis <flavio.depedis@gmail.com>
2017-10-25 14:17:23 +0200

    Update to release 2.0

--- app/src/main/java/it/flaviodepedis/meteoflax/activity/MainActivity.java
+++ app/src/main/java/it/flaviodepedis/meteoflax/activity/MainActivity.java
@@ -1,27 +1,61 @@
 package it.flaviodepedis.meteoflax.activity;
 
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.flaviodepedis.meteoflax.R;
-import it.flaviodepedis.meteoflax.common.GPSTracker;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
 
-    //private final String ERROR_MESSAGE_EMPTY = getResources().getString(R.string.hint_insert_city_label);
-    private final String ERROR_MESSAGE_EMPTY = "Insert city aame";
-    private String city;
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private final String EMPTY_MESSAGE_EMPTY = "Empty city name";
+
+    private final static int ALL_PERMISSIONS_RESULT = 101;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+
+    LocationManager locationManager;
+    Location loc;
+    ArrayList<String> permissions = new ArrayList<>();
+    ArrayList<String> permissionsToRequest;
+    ArrayList<String> permissionsRejected = new ArrayList<>();
+    boolean isGPS = false;
+    boolean isNetwork = false;
+    boolean canGetLocation = true;
+    private String city = null;
 
     @BindView(R.id.btnSubmit)
     Button btnSearchCity;
@@ -30,31 +64,14 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.swipeContainer)
     SwipeRefreshLayout swipeContainer;
 
-    // GPSTracker class
-    private GPSTracker gps;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        // Create class object for get Location throught constructor
-        gps = new GPSTracker(MainActivity.this);
-
-        // check if GPS enabled
-        if (gps.canGetLocation()) {
-
-            // Set city name into the EditText
-            setEditText(gps);
-        } else {
-            // can't get location
-            // GPS or Network is not enabled
-            // Ask user to enable GPS/network in settings
-            gps.showSettingsAlert();
-        }
-
+        // Set city name to null to clear
+        etCity.setText(null);
 
         // click search button
         btnSearchCity.setOnClickListener(new View.OnClickListener() {
@@ -68,10 +85,12 @@ public class MainActivity extends AppCompatActivity {
                     settingsIntent.putExtra("cityMeteo", city);
                     startActivity(settingsIntent);
                 } else {
-                    Toast.makeText(getApplicationContext(), ERROR_MESSAGE_EMPTY, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), EMPTY_MESSAGE_EMPTY, Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+        swipeContainer.setColorSchemeColors(Color.RED, Color.GREEN, Color.BLUE, Color.CYAN);
 
         // Setup refresh listener which triggers new data loading
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -84,19 +103,28 @@ public class MainActivity extends AppCompatActivity {
                 // Set city name to null to clear
                 etCity.setText(null);
 
-                // call constructor
-                gps = new GPSTracker(MainActivity.this);
+                // get location
+                getLocation();
 
                 // set EditText
-                setEditText(gps);
+                if (!TextUtils.isEmpty(city)) {
+                    setEditText(city);
+                }
 
                 swipeContainer.setRefreshing(false);
             }
         });
+
+        // get location
+        getLocation();
+
+        // set EditText
+        if (!TextUtils.isEmpty(city)) {
+            setEditText(city);
+        }
     }
 
-    public void setEditText(GPSTracker gps) {
-        city = gps.getCityName();
+    public void setEditText(String city) {
         etCity.setText(city);
     }
 
@@ -115,5 +143,229 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(LOG_TAG, "onLocationChanged");
+        updateUI(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        getLocation();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    private void getLocation() {
+        try {
+
+            locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+            isGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            permissionsToRequest = findUnAskedPermissions(permissions);
+
+            if (!isGPS && !isNetwork) {
+                Log.d(LOG_TAG, "Connection off");
+                showSettingsAlert();
+                //getLastLocation();
+            } else {
+                Log.d(LOG_TAG, "Connection on");
+                // check permissions
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (permissionsToRequest.size() > 0) {
+                        requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                                ALL_PERMISSIONS_RESULT);
+
+                        Log.d(LOG_TAG, "Permission requests");
+                        canGetLocation = true;
+                    }
+                }
+            }
+
+            if (canGetLocation) {
+                Log.d(LOG_TAG, "Can get location");
+                if (isNetwork) {
+                    // from Network Provider
+                    Log.d(LOG_TAG, "NETWORK_PROVIDER on");
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    if (locationManager != null) {
+                        loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (loc != null)
+                            updateUI(loc);
+                    }
+                } else if (isGPS) {
+                    // from GPS
+                    Log.d(LOG_TAG, "GPS on");
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    if (locationManager != null) {
+                        loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (loc != null)
+                            updateUI(loc);
+                    }
+                } else {
+                    Toast.makeText(this, "No location found - Activate GPS", Toast.LENGTH_SHORT).show();
+                    etCity.setText(null);
+                }
+            } else {
+                Log.d(LOG_TAG, "Can't get location");
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateUI(Location loc) {
+        Log.d(LOG_TAG, "updateUI");
+
+        //------- To get city name from coordinates --------
+        Geocoder gcd = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            if (addresses != null && addresses.size() > 0) {
+                //System.out.println(addresses.get(0).getLocality());
+                city = addresses.get(0).getLocality();
+            } else {
+                Toast.makeText(this, "No location info", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    private void getLastLocation() {
+        try {
+            Criteria criteria = new Criteria();
+            String provider = locationManager.getBestProvider(criteria, false);
+            Location location = locationManager.getLastKnownLocation(provider);
+            Log.d(LOG_TAG, provider);
+            Log.d(LOG_TAG, location == null ? "NO LastLocation" : location.toString());
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList result = new ArrayList();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean hasPermission(String permission) {
+        if (canAskPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canAskPermission() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case ALL_PERMISSIONS_RESULT:
+                Log.d(LOG_TAG, "onRequestPermissionsResult");
+                for (String perms : permissionsToRequest) {
+                    if (!hasPermission(perms)) {
+                        permissionsRejected.add(perms);
+                    }
+                }
+
+                if (permissionsRejected.size() > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                            showMessageOKCancel("These permissions are mandatory for the application. Please allow access.",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(permissionsRejected.toArray(
+                                                        new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+                } else {
+                    Log.d(LOG_TAG, "No rejected permissions.");
+                    canGetLocation = true;
+                    getLocation();
+                }
+                break;
+        }
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("GPS is not Enabled!");
+        alertDialog.setMessage("Do you want to turn on GPS?");
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                city = null;
+                etCity.setText(null);
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
